502b3e8006fa2fc4f96d317df3fdbf926305b508
Claudio Tessaro <claudio0190@hotmail.com>
2020-05-30 00:41:19 -0300

    Criação da base de produtos

---
+++ src/app/database/migrations/20200530033508-create-produtos.js
@@ -0,0 +1,58 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("produtos", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      marca_produto: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      quantidade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      valor_venda: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      valor_compra: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      data_da_compra: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      id_tipo_produto: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "tipo_produtos", key: "id" },
+        onUpdate: "CASCADE",
+      },
+      quantidade_estoque: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable("produtos");
+  },
+};
